194c53d922aa369e5712d09fda3e3157aa2f8c39
Aldy Naufal <aldy.naufal02@gmail.com>
2025-06-13 19:41:12 +0700

    remove machine learning

--- machine-learning/collaborative-learning/config/_init_.py
+++
@@ -1,8 +0,0 @@
-# config/__init__.py
-"""
-Configuration package for Recipe Recommendation API
-"""
-
-from .config import Config, DevelopmentConfig, ProductionConfig, TestingConfig
-
-__all__ = ['Config', 'DevelopmentConfig', 'ProductionConfig', 'TestingConfig']
